5913f671da1bdac975d92a500aa20f373f6a8c59
Andrew N. Shalaev <isqad88@yandex.ru>
2017-08-30 22:30:29 +0500

    initial

---
+++ src/shop.hs
@@ -0,0 +1,35 @@
+module Shop where
+
+data Client = GovOrg String Address
+            | Company String Integer String String Address
+            | Individual Person String Bool Address
+            | OnlineStore String String Address
+            deriving Show
+
+data Address = Address Country City Street Integer
+  deriving Show
+
+data Country = Country String
+  deriving Show
+
+data City = City String
+  deriving Show
+
+data Street = Street String
+  deriving Show
+
+data Person = Person String String Gender
+  deriving Show
+
+data Gender = Male | Female | Unknown
+  deriving Show
+
+data TimeMachine = FutureJourney Manufacturer Integer Price
+                 | PastJourney Manufacturer Integer Price
+                 deriving Show
+
+data Manufacturer = Manufacturer Client
+  deriving Show
+
+data Price = Price Double String
+  deriving Show
