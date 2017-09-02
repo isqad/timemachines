b50f0ee002fd86d81eea90afec6bc79f97a4378c
Andrew N. Shalaev <isqad88@yandex.ru>
2017-09-02 22:13:48 +0500

    exercise2.5: gender statistics of clients

--- src/shop.hs
+++ src/shop.hs
@@ -1,9 +1,9 @@
 module Shop where
 
 data Client = GovOrg String Address
-            | Company String Integer String String Address
+            | Company String Integer String String Person Address
             | Individual Person String Bool Address
-            | OnlineStore String String Address
+            | OnlineStore String String Person Address
             deriving Show
 
 data Address = Address Country City Street Integer
@@ -36,4 +36,35 @@ data Price = Price Double Currency
 
 data Currency = Currency String
   deriving Show
+
+address = Address (Country "Russia") (City "Moscow") (Street "Lenina") 13
+client1 = GovOrg "NT" address
+client2 = Individual (Person "Andrew" "Shalaev" Male) "Foo bar" False address
+client3 = Company "Cows" 1 "foo" "bar" (Person "Lisa" "Hacker" Female) address
+client4 = Company "Cows2" 1 "foo" "bar" (Person "Lisa" "Hacker" Female) address
+
+clientGender :: Client -> Gender
+clientGender (Individual (Person _ _ gender) _ _ _) = gender
+clientGender (OnlineStore _ _ (Person _ _ gender) _) = gender
+clientGender (Company _ _ _ _ (Person _ _ gender) _) = gender
+clientGender _ = Unknown
+
+clientGenders :: [Client] -> [Gender]
+clientGenders = map clientGender
+
+data GenderStatistic = GenderStatistic Integer Integer Integer
+  deriving Show
+
+genderStatIncr :: Gender -> GenderStatistic -> GenderStatistic
+genderStatIncr (Female) (GenderStatistic males females unknowns) = GenderStatistic males (females + 1) unknowns
+genderStatIncr (Male) (GenderStatistic males females unknowns) = GenderStatistic (males + 1) females unknowns
+genderStatIncr (Unknown) (GenderStatistic males females unknowns) = GenderStatistic males females (unknowns + 1)
+
+genderStatistic :: [Gender] -> GenderStatistic -> GenderStatistic
+genderStatistic [] stat = stat
+genderStatistic (x:xs) stat
+  = genderStatistic xs (genderStatIncr x stat)
+
+clientGendersStatistic :: [Client] -> GenderStatistic
+clientGendersStatistic clients = genderStatistic (clientGenders clients) (GenderStatistic 0 0 0)
 
